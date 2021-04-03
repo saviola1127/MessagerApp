6441727c22c8bdda691887280a705f08a63b3a6a
Savy <saviola1127@gmail.com>
2021-04-04 01:12:47 +0800

    update external storage permission issue for Android 6+

--- Messager/common/src/main/java/com/savypan/italker/common/app/CommonActivity.java
+++ Messager/common/src/main/java/com/savypan/italker/common/app/CommonActivity.java
@@ -1,7 +1,9 @@
 package com.savypan.italker.common.app;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -94,23 +96,15 @@ public abstract class CommonActivity extends AppCompatActivity {
         return super.onSupportNavigateUp();
     }
 
-    private static final int REQUEST_EXTERNAL_STORAGE = 1;
-
-    private static String[] PERMISSIONS_STORAGE = {
-            "android.permission.READ_EXTERNAL_STORAGE",
-            "android.permission.WRITE_EXTERNAL_STORAGE" };
-
     public static void verifyStoragePermissions(Activity activity) {
         try {
-            //检测是否有写的权限
-            int permission = ActivityCompat.checkSelfPermission(activity,
-                    "android.permission.WRITE_EXTERNAL_STORAGE");
-            if (permission != PackageManager.PERMISSION_GRANTED) {
-                // 没有写的权限，去申请写的权限，会弹出对话框
-                ActivityCompat.requestPermissions(activity, PERMISSIONS_STORAGE,REQUEST_EXTERNAL_STORAGE);
-                System.out.println("here1 ");
-            }else {
-                System.out.println("here2 ");
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && activity.checkSelfPermission(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                activity.requestPermissions(new String[]{
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE
+                }, 1);
             }
         } catch (Exception e) {
             e.printStackTrace();
