25cf2e0f23b0c346114e29fa9a7ead4ee03a1863
Savy <saviola1127@gmail.com>
2021-04-10 01:06:48 +0800

    import some utils class

--- Messager/common/src/main/java/com/savypan/italker/common/app/CommonApplication.java
+++ Messager/common/src/main/java/com/savypan/italker/common/app/CommonApplication.java
@@ -2,6 +2,11 @@ package com.savypan.italker.common.app;
 
 import android.app.Application;
 import android.os.SystemClock;
+import android.support.annotation.StringRes;
+import android.widget.Toast;
+
+import net.qiujuer.genius.kit.handler.Run;
+import net.qiujuer.genius.kit.handler.runable.Action;
 
 import java.io.File;
 
@@ -23,6 +28,10 @@ public class CommonApplication extends android.app.Application {
         return app.getCacheDir();
     }
 
+    /***
+     * 获取头像的临时存储地址
+     * @return
+     */
     public static File getPortraitTmpFile() {
         //得到头像目录的缓存地址
         File dir = new File(getCacheDirFile(), "portrait");
@@ -36,9 +45,51 @@ public class CommonApplication extends android.app.Application {
             }
         }
 
-
         //返回一个当前时间戳的目录文件地址
         File path = new File(dir, SystemClock.uptimeMillis() + ".jpg");
         return path.getAbsoluteFile();
+    }
+
+
+    /***
+     * 获取音乐文件的本地存储地址
+     * @return
+     */
+    public static File getAudioTmpFile(boolean isTmp) {
+        //得到头像目录的缓存地址
+        File dir = new File(getCacheDirFile(), "audio");
+        //创建所有的对应的文件夹
+        dir.mkdirs();
+
+        File[] files = dir.listFiles();
+        if (files != null && files.length > 0) {
+            for (File file : files) {
+                file.delete();
+            }
+        }
+
+        //aar
+        File path = new File(getCacheDirFile(), isTmp? "tmp.mp3":SystemClock.uptimeMillis() + ".mp3");
+        return path.getAbsoluteFile();
+    }
+
+
+    public static Application getInstance() {
+        return app;
+    }
+
+
+    public static void showToast(final String msg) {
+        Run.onUiAsync(new Action() {
+            @Override
+            public void call() {
+                Toast.makeText(app, msg, Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    public static void showToast(@StringRes int msgId) {
+        showToast(app.getString(msgId));
     }
 }
